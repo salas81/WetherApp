bae877deea51b9fc9ab024d65c0f721811d54447
Lorenzo Decaria <lorenzo.decaria@gmail.com>
2024-11-24 20:39:50 -0600

    Added section for detail view

---
+++ WetherApp/View/WeatherDetailView/Sections/OverviewSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  OverviewSectionView.swift
+//  WetherApp
+//
+//  Created by Lorenzo Decaria on 11/24/24.
+//
+
+import SwiftUI
+
+struct OverviewSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OverviewSectionView()
+}
---
+++ WetherApp/View/WeatherDetailView/Sections/OverviewSectionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OverviewSectionViewModel.swift
+//  WetherApp
+//
+//  Created by Lorenzo Decaria on 11/24/24.
+//
+
+import Foundation
