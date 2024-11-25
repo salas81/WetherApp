4389e600166ed9e51423fe5b70ae3399a50a0f82
Lorenzo Decaria <lorenzo.decaria@gmail.com>
2024-11-24 20:40:20 -0600

    Added section for detailed view

--- WetherApp/View/WeatherDetailView/Sections/OverviewSectionView.swift
+++ WetherApp/View/WeatherDetailView/Sections/OverviewSectionView.swift
@@ -9,9 +9,44 @@ import SwiftUI
 
 struct OverviewSectionView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            cityView
+            temperatureView
+        }
+        .padding(.horizontal, 40)
     }
 }
+
+private extension OverviewSectionView {
+    var cityView: some View {
+        HStack {
+            Text("Prague")
+                .font(.headline)
+            Spacer()
+        }
+    }
+    
+    var temperatureView: some View {
+        HStack(alignment: .top) {
+            Text("17°C")
+                .font(.system(size: 80))
+                .bold()
+            Image(systemName: "star.fill")
+                .padding(.top, 15)
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("Sunny")
+                    .font(.caption)
+                    .padding(.top, 20)
+                Spacer()
+                Text("Feels like 16°C")
+                    .font(.caption)
+            }
+        }
+        .frame(height: 80)
+    }
+}
+
 
 #Preview {
     OverviewSectionView()
